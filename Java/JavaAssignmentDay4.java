5c506d9127058c2e02ae8563d948a88eff213bb1
Balraj Singh <balraj0496@gmail.com>
2018-01-12 11:28:57 +0530

    Remove unnecessary imports

--- Java/JavaAssignmentDay4.java
+++ Java/JavaAssignmentDay4.java
@@ -1,5 +1,13 @@
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+/**
+ * Create three new types of exceptions. Write a class with a method that throws all three.
+ * In main( ), call the method but only use a single catch clause that will catch all three
+ * types of exceptions. Add a finally clause and verify that your finally clause is executed,
+ * even if a NullPointerException is thrown.
+  */
 
+/**
+ * @author Balraj Singh
+ */
 class CustomExceptionA extends Exception {
     CustomExceptionA(String message) {
         super(message);
